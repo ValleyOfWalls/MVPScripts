18058b967f9047ace3924d7858b6ad03dfc1d216
Lee Gracey <llgracey@gmail.com>
2025-05-04 23:36:31 -0500

    asd

--- Assets/MVPScripts/Base/Card.cs
+++
@@ -1,6 +0,0 @@
-using Unity.Netcode;
-
-public class Card : NetworkBehaviour
-{
-    public new CombatPlayer Owner;
-} 
--- Assets/MVPScripts/Combat/CombatCanvasManager.cs
+++
@@ -1,9 +0,0 @@
-// ... existing code ...
-    {
-        NetworkObject targetPlayerNetworkObject = NetworkManager.Singleton.SpawnManager.GetPlayerNetworkObject(targetClientId);
-        CombatPlayer targetPlayer = targetPlayerNetworkObject.GetComponent<CombatPlayer>();
-        if (targetPlayer != null && targetPlayer.IsOwner && targetPlayer.IsServerInitialized)
-        {
-            targetPlayer.CmdDiscardCardServerRpc(cardInstanceID, targetPlayerId);
-        }
-// ... existing code ...
